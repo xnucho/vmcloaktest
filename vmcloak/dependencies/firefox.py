f3cb53da34ea33296b66124e1753e4a48daee29c
dfir-alvin <53186297+dfir-alvin@users.noreply.github.com>
2024-10-08 19:37:21 -0500

    Update firefox.py
    Updated URL to Firefox ESR for support of Win 7 and Win 10 images

--- vmcloak/dependencies/firefox.py
+++ vmcloak/dependencies/firefox.py
@@ -7,26 +7,19 @@ from vmcloak.abstract import Dependency
 
 class Firefox(Dependency):
     name = "firefox"
-    default = "41.0.2"
+    default = "128.3.0esr"
     tags = ["browser_firefox"]
     exes = [{
-        "version": "41.0.2",
-        "url": "https://cuckoo.sh/vmcloak/Firefox_Setup_41.0.2.exe",
+        "version": "128.3.0esr",
+        "url": "https://download.mozilla.org/?product=firefox-esr-msi-latest-ssl&os=win64&lang=en-US",
         "sha1": "c5118ca76f0cf6ecda5d2b9292bf191525c9627a",
-    }, {
-        "version": "60.0.2",
-        "url": "https://cuckoo.sh/vmcloak/firefox_60_0_2.exe",
-        "sha1": "565c5ddca3e4acbc30a550f312d3a1a7fd8d8bce",
-    }, {
-        "version": "63.0.3",
-        "url": "https://cuckoo.sh/vmcloak/firefox_63_0_3.exe",
-        "sha1": "c5f03fc93aebd2db9da14ba6eb1f01e98e18d95b",
     }]
 
     def run(self):
-        self.upload_dependency("C:\\Firefox_Setup_41.0.2.exe")
-        self.a.execute("C:\\Firefox_Setup_41.0.2.exe -ms")
-        self.a.remove("C:\\Firefox_Setup_41.0.2.exe")
+        self.upload_dependency("C:\\%s" % self.filename)
+
+        self.a.execute("msiexec /i C:\\%s /passive /norestart" % self.filename)
+        self.a.remove("C:\\%s" % self.filename)
 
 class Firefox41(Firefox, Dependency):
     """Backwards compatibility"""
